81f88952714e4698704438707875e7ef5ca127f5
Konrad Korf <konrad.korf@gmail.com>
2021-02-04 20:46:32 +0100

    Appending works

--- data_handler.py
+++ data_handler.py
@@ -11,6 +11,14 @@ def get_all_user_stories():
 
     return all_stories
 
+def add_user_story(story):
+    story['id'] = get_next_id()
+    story['status'] = STATUSES[1]
+
+    with open(DATA_FILE_PATH, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=DATA_HEADER)
+        writer.writerow(story)
+
 def get_csv_data():
     data = []
     with open(DATA_FILE_PATH, encoding='utf-8') as file:
@@ -19,3 +27,11 @@ def get_csv_data():
             file_row = dict(row)
             data.append(file_row)
     return data
+
+def get_next_id():
+    existing_data = get_all_user_stories()
+
+    if len(existing_data) == 0:
+        return '1'
+
+    return str(int(existing_data[-1]['id']) + 1)
